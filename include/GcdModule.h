819d3e325bfcde38d19730fb6db7a4ca480b4949
PrefectSol <prefectsol@mail.ru>
2023-02-04 02:44:51 +0500

    Add new

--- include/GcdModule.h
+++ include/GcdModule.h
@@ -2,14 +2,23 @@
 #define GCDMODULE_H
 
 #include <iostream>
+#include <vector>
 #include <filesystem>
 #include <fstream>
 
 #include "Network.h"
 #include "Algorithm.h"
 
-uint32_t runGcdModule(int argc, char **argv);
+const int numLimit = 100000;
+
+uint32_t runGcdModule(int argc, const char **argv);
+
+uint32_t initData(int argc, const char **argv, int countData, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData);
+
+void createGcdData(int countData, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData);
 
 uint32_t createGcdData(int countData, std::string filename);
+
+uint32_t readGcdDataFile(std::string filename, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData);
 
 #endif // !GCDMODULE_H
--- src/GcdModule.cpp
+++ src/GcdModule.cpp
@@ -1,8 +1,8 @@
 #include "GcdModule.h"
 
-uint32_t runGcdModule(int argc, char **argv)
+uint32_t runGcdModule(int argc, const char **argv)
 {
-    if (argc != 4)
+    if (argc < 3)
     {
         return ErrorCode::notEnoughArguments;
     }
@@ -14,11 +14,39 @@ uint32_t runGcdModule(int argc, char **argv)
         return ErrorCode::cannotConvertStrToInt;
     }
 
-    uint32_t isCreateGcdData = createGcdData(countData, argv[3]);
-    if (isCreateGcdData != ErrorCode::success)
+    std::vector<std::pair<int, int>> inputData;
+    std::vector<int> outputData;
+    
+    uint32_t isInitData = initData(argc, argv, countData, &inputData, &outputData);
+    if (isInitData != ErrorCode::success)
     {
-        return isCreateGcdData;
+        return isInitData;
     }
+
+
+    // run nn training...
+    // ...
+
+
+    return ErrorCode::success;
+}
+
+uint32_t initData(int argc, const char **argv, int countData, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData)
+{
+    if (argc > 3 && isPathExists(argv[3]))
+    {
+        uint32_t isCreateGcdData = createGcdData(countData, argv[3]);
+        if (isCreateGcdData != ErrorCode::success)
+        {
+            return isCreateGcdData;
+        }
+
+        uint32_t isReadGcdDataFile = readGcdDataFile(argv[3], inputData, outputData);
+        
+        return isReadGcdDataFile;
+    }   
+
+    createGcdData(countData, inputData, outputData);
 
     return ErrorCode::success;
 }
@@ -33,8 +61,6 @@ uint32_t createGcdData(int countData, std::string filename)
     }
 
     srand(time(NULL));
-    const int numLimit = 10000;
-
     for(int i = 0; i < countData; i++)
     {
         int num1 = rand() % numLimit;
@@ -44,6 +70,46 @@ uint32_t createGcdData(int countData, std::string filename)
         
         writer << num1 << " " << num2 << " " << gcd << '\n';
     }   
+
+    writer.close();
+
+    return ErrorCode::success;
+}
+
+void createGcdData(int countData, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData)
+{
+    srand(time(NULL));
+    for(int i = 0; i < countData; i++)
+    {
+        int num1 = rand() % numLimit;
+        int num2 = rand() % numLimit;
+        
+        int gcd = getGcd(num1, num2);
+        
+        inputData->push_back(std::make_pair(num1, num2));
+        outputData->push_back(gcd);
+    }   
+}
+
+uint32_t readGcdDataFile(std::string filename, std::vector<std::pair<int, int>> *inputData, std::vector<int> *outputData)
+{
+    std::ifstream reader(filename);
+
+    if(!reader.is_open())
+    {
+        return ErrorCode::cannotOpenFile;
+    }
+
+    int num1, num2, gcd;
+    while(!reader.eof())
+    {
+        reader >> num1 >> num2 >> gcd;
+
+        inputData->push_back(std::make_pair(num1, num2));
+        outputData->push_back(gcd);
+    }
+
+    reader.close();
 
     return ErrorCode::success;
 }
